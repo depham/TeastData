c2de5d63c4af65a796109446648598d5464a5848
depham00172 <ngocdemr@gmail.com>
2023-05-28 22:23:56 +0700

    C5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+
+
 /*Body parser */
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -8,6 +10,10 @@ app.use(bodyParser.json());
 
 require('./app/routers/home.router')(app);
 require('./app/routers/dulieutron.router')(app);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(3000, function(){
     console.log("Server is 3000");
